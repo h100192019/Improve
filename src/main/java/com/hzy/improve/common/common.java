042af7016133c161609e7317688cb68a5630b9b5
hzy <100192019@qq.com>
2018-01-31 17:26:29 +0800

    common

---
+++ src/main/java/com/hzy/improve/common/common.java
@@ -0,0 +1,11 @@
+package com.hzy.improve.common;
+
+/**
+ * Created by eju on 2018/1/31.
+ */
+public class common {
+
+    public static void main(String[] args) {
+        System.out.println("--开始--");
+    }
+}
